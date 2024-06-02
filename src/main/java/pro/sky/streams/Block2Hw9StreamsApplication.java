671d7f9cbbbe2e6d43d06e8e388b2d991ed1ae65
Ed <eduard@kamenskikh.me>
2024-06-03 03:00:07 +0400

    Initial Commit

---
+++ src/main/java/pro/sky/streams/Block2Hw9StreamsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block2Hw9StreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block2Hw9StreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/streams/Block2Hw9StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block2Hw9StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
